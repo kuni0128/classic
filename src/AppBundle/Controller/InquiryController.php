7082ee06f5ac5d9c6c874fb70956eb64c661ef62
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2017-07-29 23:13:08 +0900

    Add inquiry form

---
+++ src/AppBundle/Controller/InquiryController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/inquiry")
+ *
+ */
+class InquiryController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method("get")
+     */
+    public function indexAction()
+    {
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $this->createInquiryForm()->createView()]
+        );
+    }
+
+    /**
+     * @Route("/complete")
+     */
+    public function completeAction()
+    {
+        return $this->render('Inquiry/complete.html.twig');
+    }
+
+    /**
+     * @Route("/")
+     * @Method("post")
+     */
+    public function indexPostAction(Request $request)
+    {
+        $form = $this->createInquiryForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            return $this->redirect($this->generateUrl('app_inquiry_complete'));
+        }
+
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    private function createInquiryForm()
+    {
+        return $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->add('tel', TextType::class, [
+                'required' => false,
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    '公演について',
+                    'その他',
+                ],
+                'expanded' => true,
+            ])
+            ->add('content', TextareaType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => '送信',
+            ])
+            ->getForm();
+    }
+}
